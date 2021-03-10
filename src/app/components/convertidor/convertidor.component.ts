a22b68cd10db38734fc64375044922ec0a9bce90
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2021-03-09 22:43:36 -0300

    first commit

---
+++ src/app/components/convertidor/convertidor.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convertidor',
+  templateUrl: './convertidor.component.html',
+  styleUrls: ['./convertidor.component.css'],
+})
+export class ConvertidorComponent implements OnInit {
+  cantidad = 0;
+  tengo = 'USD';
+  quiero = 'EUR';
+  total = 0;
+
+  monedas: string[] = ['USD', 'EUR', 'LIBRA'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  convertir() {
+    switch (this.tengo) {
+      case 'USD':
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad;
+        }
+        if (this.quiero === 'EUR') {
+          this.total = this.cantidad * 0.84;
+        }
+        if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad * 0.72;
+        }
+        break;
+
+      case 'EUR':
+        if (this.quiero === 'EUR') {
+          this.total = this.cantidad;
+        }
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad * 1.19;
+        }
+        if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad * 0.86;
+        }
+        break;
+
+      case 'LIBRA':
+        if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad;
+        }
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad * 1.39;
+        }
+        if (this.quiero === 'EUR') {
+          this.total = this.cantidad * 1.17;
+        }
+        break;
+    }
+  }
+}
